cc406c3d5e8a2486e7083f052ce2990324ceaebf
Duff Chagas <daisy.chagas@cognizant.com>
2022-02-11 16:18:47 +0800

    MAQS Quest Session 1 homework

---
+++ MAQS/Session1/Tests/Session1Homework.cs
@@ -0,0 +1,17 @@
+﻿using CognizantSoftvision.Maqs.BaseTest;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class Session1Homework : BaseTest
+    {
+
+        [TestMethod]
+        public void SampleTestSession1()
+        {
+            this.TestObject.Log.LogMessage("Start Session 1 Test");
+            Assert.IsTrue(true, "True is Not True");
+        }
+    }
+}
