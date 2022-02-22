a964abf44b2852ce1e1a7ca1d4a21d1d25ce117e
Duff Chagas <daisy.chagas@cognizant.com>
2022-02-22 19:31:50 +0800

    MAQS Quest Homework No. 3

---
+++ Session3Homework/Session3Homework/Tests/ArrayOfProductsTest.cs
@@ -0,0 +1,25 @@
+﻿using CognizantSoftvision.Maqs.BaseWebServiceTest;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebServiceModel;
+
+namespace Tests
+{
+    /// <summary>
+    /// Simple web service test class using VS unit
+    /// </summary>
+    [TestClass]
+    public class ArrayOfProductsTest : BaseWebServiceTest
+    {              
+        [TestMethod]
+        public void GetAllProducts_ShouldReturnSuccess()
+        {
+            //Override the WebServiceDriver and add the key "Auth" and value "AuthKey" to the DEFAULT request header
+            this.WebServiceDriver.HttpClient.DefaultRequestHeaders.Add("Auth", "AuthKey");
+
+            //Create a JSON test that gets from api/XML_JSON/GetAllProducts and save the result as an ArrayOfProducts
+
+            ArrayOfProductsModel result = this.WebServiceDriver.Get<ArrayOfProductsModel>("/api/XML_JSON/GetAllProducts", "application/json", false);
+            Assert.AreEqual(3, result.Count);
+        }
+    }
+}
---
+++ Session3Homework/Session3Homework/WebServiceModel/ArrayOfProductsModel.cs
@@ -0,0 +1,9 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace WebServiceModel
+{
+    //Generate an ArrayOfProducts C# Object Model
+    public class ArrayOfProductsModel : List<ProductJson> { }
+}
+   
